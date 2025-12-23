1bfa7746dbe44cb12b5fa2428ed8d0bc219d1e94
Giorgi Chitiashvili <snipnapgamer@gmail.com>
2025-12-23 20:07:20 +0400

    burgerMenu

--- Tierlist.js
+++ Tierlist.js
@@ -1,11 +1,10 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("hamburger");
     const mobileNav = document.getElementById("mobileNav");
 
     if (!hamburger || !mobileNav) return;
 
-    hamburger.addEventListener("click", () => {
+    hamburger.addEventListener("click", function () {
         hamburger.classList.toggle("active");
         mobileNav.classList.toggle("active");
     });
